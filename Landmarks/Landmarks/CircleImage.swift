91702947b1fdbd931964e3eefa235ffc7310dbba
KaitoMuraoka <kaitoboshi@icloud.com>
2022-06-30 23:38:49 +0900

    少し編集

--- Landmarks/Landmarks/CircleImage.swift
+++ Landmarks/Landmarks/CircleImage.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
-            .clipShape(Circle())
-            .overlay{
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
                 Circle().stroke(.white, lineWidth: 4)
             }
             .shadow(radius: 7)
--- Landmarks/Landmarks/MapView.swift
+++ Landmarks/Landmarks/MapView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 import MapKit
 
+import SwiftUI
+import MapKit
+
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
     )
-    
+
     var body: some View {
         Map(coordinateRegion: $region)
     }
